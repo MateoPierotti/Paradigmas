ec3a63ae1245ccc806981b2c042d56984fe38a30
UADER_IS2_PIEROTTI <127799431+MateoPierotti@users.noreply.github.com>
2024-07-24 19:42:49 -0300

    Update reales.py

--- reales.py
+++ reales.py
@@ -1,15 +1,15 @@
 def es_real(lexema):
     Q0 = 0
     Q = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    F = [6, 7]
+    F = [5, 6]
 
     estado_actual = Q0
     indice = 0
 
     SIGMA = {
-        "DIGITO": 0,
+        "MENOS": 0,
         "MAS": 1,
-        "MENOS": 2,
+        "DIGITO": 2,
         "PUNTO": 3,
         "e": 4,
         "E": 5,
@@ -17,15 +17,15 @@ def es_real(lexema):
     }
 
     DELTA = [
-        [2, 1, 1, 3, 8, 8, 8], # transiciones de Q0
-        [2, 8, 8, 3, 8, 8, 8], # transiciones de Q1
-        [2, 8, 8, 6, 8, 8, 8], # transiciones de Q2
-        [6, 8, 8, 8, 8, 8, 8], # transiciones de Q3
-        [7, 8, 8, 8, 8, 8, 8], # transiciones de Q4
-        [7, 4, 4, 8, 8, 8, 8], # transiciones de Q5
-        [6, 8, 8, 8, 5, 5, 8], # transiciones de Q6
-        [7, 8, 8, 8, 8, 8, 8], # transiciones de Q7
-        [8, 8, 8, 8, 8, 8, 8]  # transiciones de Q8 (estado de rechazo)
+        [1, 1, 2, 7, 8, 8, 8], # transiciones de Q0
+        [8, 8, 2, 7, 8, 8, 8], # transiciones de Q1
+        [8, 8, 2, 5, 4, 4, 8], # transiciones de Q2
+        [8, 8, 6, 8, 8, 8, 8], # transiciones de Q3
+        [3, 3, 6, 8, 8, 8, 8], # transiciones de Q4
+        [8, 8, 5, 8, 4, 4, 8], # transiciones de Q5
+        [8, 8, 6, 8, 8, 8, 8], # transiciones de Q6
+        [8, 8, 5, 8, 8, 8, 8],  # transiciones de Q7
+        [8, 8, 8, 8, 8, 8, 8] # transiciones de Q8
     ]
 
     def simbolo(caracter):
